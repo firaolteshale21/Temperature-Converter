0253a8e7a58f2c90a98051e38c8f504a8c4c0042
Firaol Teshale <102233709+firaolteshale21@users.noreply.github.com>
2024-05-24 18:52:06 +0300

    Add files via upload

---
+++ temp_conv.js
@@ -0,0 +1,43 @@
+const fahrenheitToCelsius = document.getElementById("fahrenheitTocelsius");
+const celsiusToFahrenheit = document.getElementById("Celsiustofahrenheit");
+const answer = document.getElementById("answer");
+
+function convert() {
+  const userInput = parseFloat(document.getElementById("userinput").value);
+  let temp;
+
+  if (fahrenheitToCelsius.checked) {
+    answer.style.color = 'black';
+    temp = (userInput - 32) * 5 / 9;
+    answer.textContent = temp.toFixed(1) + " °C";
+  } else if (celsiusToFahrenheit.checked) {
+    answer.style.color = 'black';
+    temp = (userInput * 9 / 5) + 32;
+    answer.textContent = temp.toFixed(1) + " °F";
+  }else if(isNaN(userInput)){
+    answer.textContent = "Empty Input";
+    answer.style.color = 'red';
+  }
+  else{
+    answer.style.color = 'red';
+    answer.textContent = "Select Conversion"
+  }
+}
+function reset() {
+    document.getElementById("userinput").value = "";
+    answer.textContent = "";
+    document.getElementById("fahrenheitTocelsius").checked = false;
+    document.getElementById("Celsiustofahrenheit").checked = false;
+  }
+  function swap(){
+    if(fahrenheitToCelsius.checked){
+        document.getElementById("fahrenheitTocelsius").checked = false;
+        document.getElementById("Celsiustofahrenheit").checked = true;
+
+    }else if(celsiusToFahrenheit.checked){
+        document.getElementById("fahrenheitTocelsius").checked = true;
+        document.getElementById("Celsiustofahrenheit").checked = false;
+    }else{
+        return;
+    }
+  }
